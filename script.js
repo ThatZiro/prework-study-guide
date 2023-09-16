3be89d2ae7c813978f641eaab01b2fe9ea2bcef5
Brandon Barnes <imbrandonbarnes@gmail.com>
2023-09-16 11:23:15 -0400

    Changed JS and Updated HTML Notes

--- script.js
+++ script.js
@@ -1,18 +1,26 @@
 var topics = ["HTML", "CSS", "Git", "JavaScript"];
-for (var i = 0; i < topics.length; i++) {
-  console.log("Let's Study " + topics[i] + "!");
+var topic = topics[Math.floor(Math.random() * topics.length)];
+listTopics();
+selectTopic();
+
+function listTopics() {
+  console.log("What should we study?");
+  for (var i = 0; i < topics.length; i++) {
+    console.log(topics[i]);
+  }
+  console.log("========================");
 }
 
-/*
-if (topic === "HTML") {
-  console.log("Let's Study HTML!");
-} else if (topic === "CSS") {
-  console.log("Let's Study CSS!");
-} else if (topic === "Git") {
-  console.log("Let's Study Git!");
-} else if (topic === "JavaScript") {
-  console.log("Let's Study JavaScript!");
-} else {
-  console.log("Invalid Topic");
+function selectTopic() {
+  if (topic === "HTML") {
+    console.log("Let's Study HTML!");
+  } else if (topic === "CSS") {
+    console.log("Let's Study CSS!");
+  } else if (topic === "Git") {
+    console.log("Let's Study Git!");
+  } else if (topic === "JavaScript") {
+    console.log("Let's Study JavaScript!");
+  } else {
+    console.log("Invalid Topic");
+  }
 }
-*/
